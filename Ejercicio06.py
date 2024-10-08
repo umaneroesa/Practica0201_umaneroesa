6c32b18752aa832bbe85169da8281b565dab5f51
umaneroesa <umaneroesa1@educacion.navarra.es>
2024-10-08 11:24:54 +0200

    ej06 Finalizado

---
+++ Ejercicio06.py
@@ -0,0 +1,4 @@
+#Escribir un programa que lea un entero positivo, , introducido por el usuario y después muestre en pantalla la suma de todos los enteros desde 1 hasta 
+n= int(input('Introduzca un numero'))
+Suma = n * (n + 1) / 2
+print  ('La suma total es ', Suma)
